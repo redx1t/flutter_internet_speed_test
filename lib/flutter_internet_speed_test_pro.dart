5810d6654749af2cdf6462eaa6a8ef56f224742b
Victor Muthomi <smartwebsafrica@gmail.com>
2025-07-09 15:59:56 +0300

    eol

--- lib/flutter_internet_speed_test_pro.dart
+++ lib/flutter_internet_speed_test_pro.dart
@@ -6,4 +6,4 @@ export './src/callbacks_enum.dart';
 export './src/flutter_internet_speed_test_platform_interface.dart'
     show ErrorCallback;
 export './src/models/server_selection_response.dart' show Client;
-export './src/test_result.dart' show TestResult; 
+export './src/test_result.dart' show TestResult;
